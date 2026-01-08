621d17e17d00ba111a54d0977b2930082f13ca6b
Anushkagill <anushkagill01@gmail.com>
2026-01-08 21:00:57 +0530

    Integrate Redux Toolkit and organize project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react' 
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import { Outlet } from 'react-router-dom'
+import { Header, Footer } from './components'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
-  return (
-    <>
-    <h1>helloo</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full-block">
+        <Header/>
+        <main>
+         TODO: {/*<outlet/>*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
