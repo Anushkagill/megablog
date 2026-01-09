fd28542046a8ef19cf78f7a304eee84370ea37f0
Anushkagill <anushkagill01@gmail.com>
2026-01-09 13:24:42 +0530

    Finalize MegaBlog: full-stack blog app with Appwrite and React

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,27 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './logo/Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
+import Signup from'./Signup'
+import Login from './Login'
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Input
 }
