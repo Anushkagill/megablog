1f8f07fb8d1be054fa9087365ce8f2f0a6474bca
Anushkagill <anushkagill01@gmail.com>
2026-01-11 22:43:15 +0530

    just fixed small error

--- src/components/index.js
+++ src/components/index.js
@@ -11,7 +11,7 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
-import Select from "./Select";
+import select from "./select";
 
 export {
   Header,
@@ -27,5 +27,5 @@ export {
   PostForm,
   AuthLayout,
   Input,
-  Select,
+  select,
 };
